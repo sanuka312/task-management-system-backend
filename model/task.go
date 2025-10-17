af4f31f193c0ac468d757f5891661950930e8eec
Sanuka <sanuka.20232509@iit.ac.lk>
2025-10-17 14:06:25 +0530

    updated commit on creating the models

--- model/task.go
+++ model/task.go
@@ -1,1 +1,17 @@
 package model
+
+import "time"
+
+type Task struct {
+	TaskId          uint      `gorm:"primaryKey" json:"task_id"`
+	TaskName        string    `gorm:"size:200;not null" json:"task_name"`
+	TaskDescription string    `gorm:"size:400;not null" json:"task_description"`
+	CreatedAt       time.Time `gorm:"autoCreateTime"`
+	TaskStatus      string    `gorm:"size:50;default:'pending'" json:"task_status"`
+	TaskDueDate     string    `json:"due_date"`
+
+	CompletedAt *time.Time `json:"completed_at"`
+
+	//Foreign keys
+	ProjectId uint `json:"project_id"`
+}
--- model/user.go
+++ model/user.go
@@ -1,5 +1,10 @@
 package model
 
 type User struct {
-	UserId uint `gorm:"primaryKey" json:"user_id"`
+	UserId       uint   `gorm:"primaryKey" json:"user_id"`
+	UserName     string `'gorm:"size:200;not null" json:"user_name"`
+	UserEmail    string `gorm:"size:250;not null" json:"user_email"`
+	UserPassword string `gorm:"size:10;not null" json:"user_password"`
+
+	Projects []Project `gorm:"foreignKey:UserId"`
 }
